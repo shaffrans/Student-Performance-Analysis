8c9806c81e389d4a88c5c75a6ece81026ce5e6a1
shaffrans <stephen.w.shaffran@leidos.com>
2022-04-18 16:14:56 -0400

    Added data labels and feature importance Added naive classifier/regressor

--- src/main/analyze.py
+++ src/main/analyze.py
@@ -6,7 +6,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import SVR, SVC
 from sklearn.metrics import mean_squared_error
 from sklearn.neural_network import MLPClassifier, MLPRegressor
+from sklearn.inspection import permutation_importance
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 # col numbers used for preprocessing
 nominal_attributes = [8, 9, 10, 11]
@@ -22,24 +25,25 @@ def load_data(subject="math", prior_grades=True):
         filename = "student-mat.csv"
     with open("../../data/" + filename) as data_file:
         reader = csv.reader(data_file, delimiter=';')
-        X, y = [], []
+        X, y, labels = [], [], []
         # number of columns to include in training set
         n_cols = 32
         if not prior_grades:
             n_cols = 30
         for i, row in enumerate(reader):
-            # skip first row
-            # TODO use first row as label
+            # use first row as label
             if i == 0:
+                labels = row[:n_cols]
                 continue
-            X.append(row[:n_cols])
-            y.append(row[-1])
-        return X, y
+            X.append([(int(n) if j in numeric_attributes else n) for j, n in enumerate(row[:n_cols])])
+            y.append(int(row[-1]))
+        return X, y, labels
 
 
 # performs normalization on numeric attributes and 1-of-K encoding on nominal attributes
-def preprocess_data(X, y, standardize=False, pass_fail=False):
+def preprocess_data(X, y, labels=None, standardize=False, pass_fail=False):
     # convert binary attributes into 0 or 1
+    labels_new = labels.copy() if labels else []
     first_row = X[0].copy()
     X_arr = np.array(X)
     for row in X_arr:
@@ -55,7 +59,7 @@ def preprocess_data(X, y, standardize=False, pass_fail=False):
     y_stand = np.array(y)
     if pass_fail:
         for i in range(len(y_stand)):
-            y_stand[i] = int(int(y_stand[i]) >= 10)
+            y_stand[i] = int(y_stand[i] >= 10)
     elif standardize:
         y_stand = scaler.fit_transform(y_stand.reshape(-1, 1)).reshape(-1)
     # one-hot encoding
@@ -65,7 +69,16 @@ def preprocess_data(X, y, standardize=False, pass_fail=False):
     # append X_hot with the encoded attributes
     for i in nominal_attributes:
         X_hot = np.append(X_hot, enc.fit_transform(X_arr[:, i].reshape(-1, 1)), axis=1)
-    return X_hot, y_stand
+        # update labels
+        if labels:
+            for name in enc.get_feature_names_out():
+                labels_new.append(labels_new[i] + ':' + name[3:])
+    labels_out = []
+    if labels:
+        for i, label in enumerate(labels_new):
+            if i not in nominal_attributes:
+                labels_out.append(label)
+    return X_hot, y_stand, labels_out
 
 
 def score(X_test, y_test, model, classify=False):
@@ -76,8 +89,20 @@ def score(X_test, y_test, model, classify=False):
         return mean_squared_error(y_test, y_pred, squared=False)
 
 
-def decision_tree(X, y, classify=False, forest=False):
-    X_new, y_new = preprocess_data(X, y, pass_fail=classify)
+# some of this code was copied from scikit-learn.org
+def feature_importances(X_test, y_test, model, labels):
+    result = permutation_importance(model, X_test, y_test, n_repeats=10, random_state=42, n_jobs=2)
+    forest_importances = pd.Series(result.importances_mean, index=labels)
+    fig, ax = plt.subplots()
+    forest_importances.plot.bar(yerr=result.importances_std, ax=ax)
+    ax.set_title("Feature importances using permutation on full model")
+    ax.set_ylabel("Mean accuracy decrease")
+    fig.tight_layout()
+    plt.show()
+
+
+def decision_tree(X, y, labels, classify=False, forest=False):
+    X_new, y_new, labels_new = preprocess_data(X, y, labels, pass_fail=classify)
     X_train, X_test, y_train, y_test = train_test_split(X_new, y_new, test_size=0.33, random_state=1)
     model_code = 2 * int(forest) + int(classify)
     if not model_code:
@@ -94,11 +119,12 @@ def decision_tree(X, y, classify=False, forest=False):
         model = RandomForestClassifier()
     model.fit(X_train, y_train)
     print(score(X_test, y_test, model, classify=classify))
+    feature_importances(X_test, y_test, model, labels_new)
 
 
 def svm(X, y, classify=False):
     # TODO set gamma value
-    X_new, y_new = preprocess_data(X, y, pass_fail=classify, standardize=True)
+    X_new, y_new, _ = preprocess_data(X, y, pass_fail=classify, standardize=True)
     X_train, X_test, y_train, y_test = train_test_split(X_new, y_new, test_size=0.33, random_state=1)
     if classify:
         model = SVC()
@@ -110,7 +136,7 @@ def svm(X, y, classify=False):
 
 def neural_network(X, y, classify=False):
     # TODO determine optimal number of hidden nodes
-    X_new, y_new = preprocess_data(X, y, pass_fail=classify, standardize=True)
+    X_new, y_new, _ = preprocess_data(X, y, pass_fail=classify, standardize=True)
     X_train, X_test, y_train, y_test = train_test_split(X_new, y_new, test_size=0.33, random_state=1)
     if classify:
         model = MLPClassifier(hidden_layer_sizes=(8,))
@@ -120,7 +146,39 @@ def neural_network(X, y, classify=False):
     print(score(X_test, y_test, model, classify=classify))
 
 
+def naive_predictor(X, y, classify=False):
+    X_new, y_new, _ = preprocess_data(X, y, pass_fail=classify)
+    X_train, X_test, y_train, y_test = train_test_split(X_new, y_new, test_size=0.33, random_state=1)
+    # if grade information is available, use that to predict performance
+    grades_available = False
+    if len(X[0]) > 30:
+        grades_available = True
+    prediction = 0
+    if not grades_available:
+        if classify:
+            prediction = np.round(np.average(y_train))
+        else:
+            prediction = np.average(y_train)
+    # set the prediction according to the previous term's grade
+    # TODO FIX THIS!
+    if grades_available:
+        if classify:
+            y_pred = [int(float(grade) >= 10) for grade in X_test[:, 27]]
+            print("grade available, classify")
+        else:
+            y_pred = X_test[:, 27].reshape(-1)
+            print("grade available, regress")
+    # if grade information is not available, predict the most likely outcome for everyone
+    else:
+        y_pred = [prediction] * len(y_test)
+        print("grade not available")
+    if classify:
+        print(sum([int(i == j) for i, j in zip(y_pred, y_test)]) / len(y_test))
+    else:
+        print(mean_squared_error(y_test, y_pred, squared=False))
+
+
 if __name__ == "__main__":
-    X, y = load_data(prior_grades=True)
-    # decision_tree(X, y, classify=True, forest=False)
-    neural_network(X, y, classify=False)
+    X, y, labels = load_data(prior_grades=True)
+    # decision_tree(X, y, labels, classify=True, forest=True)
+    naive_predictor(X, y, classify=True)
